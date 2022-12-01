90b59778607bf6804e486fa5e28dd3516e92f901
BrunoSilva02 <200221046@estudantes.ips.pt>
2022-12-01 17:56:57 +0000

    Update laboratorio7.lisp

--- Labs_Apoio_Projeto/laboratorio7.lisp
+++ Labs_Apoio_Projeto/laboratorio7.lisp
@@ -26,7 +26,20 @@
 )
 
 
-(defun get-arco-na-posicao (horizontal vertical lista)
+(defun get-arco-na-posicao (lista-arcos posicao lista)
 	"Devolve o arco existente no tabuleiro na lista de arcos horizontais ou verticais"
-		(nth (- vertical 1) (nth (- horizontal 1) lista))
+		(nth (- posicao 1) (nth (- lista-arcos 1) lista))
+)
+
+(defun substituir (indice lista &optional (valor 1))
+	(cond ((= indice 1) (cons valor (cdr lista)))
+		  ((= indice 2) (cons (car lista) (cons valor (cddr lista))))
+		  ((= indice 3) (cons (car lista) (cons (second lista)  (cons valor (cdddr lista)))))
+		  (T(cons (car lista) (cons (second lista) (cons (third lista) (cons valor ()))))))
+)
+
+(defun arco-na-posicao (lista-arcos posicao lista &optional (X 1))
+	"Coloca o um valor X no arco escolhido"
+	(substituir lista-arcos lista 
+			(substituir posicao (nth (- lista-arcos 1) lista) X))
 )
