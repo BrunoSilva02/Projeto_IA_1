2ce1acdd8c59bbd437f7dc324bb7b1c98a13b6dd
AurelioMiranda <80250266+AurelioMiranda@users.noreply.github.com>
2022-12-01 17:04:54 +0000

    copia do lab 7

---
+++ Labs_Apoio_Projeto/laboratorio7_2.lisp
@@ -0,0 +1,32 @@
+;;;; laboratorio7.lisp
+;;;; Ficha de Laborat�rio n�7 - Apoio ao 1� projeto
+;;;; Autor: 
+
+
+;;; Tabuleiro
+
+(defun tabuleiro-teste ()
+  "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
+	'(
+		((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+		((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+	)
+)
+
+;;; Exercicios
+
+(defun get-arcos-horizontais (tabuleiro)
+	"Devolve a lista dos arcos horizontais do tabuleiro"
+	(first tabuleiro)	
+)
+
+(defun get-arcos-verticais (tabuleiro)
+	"Devolve a lista dos arcos verticais do tabuleiro"
+	(second tabuleiro)
+)
+
+
+(defun get-arco-na-posicao (horizontal vertical lista)
+	"Devolve o arco existente no tabuleiro na lista de arcos horizontais ou verticais"
+		 (nth 2 lista)
+)
